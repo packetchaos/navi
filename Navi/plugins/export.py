3930d0de5c743483563f0a4a32037f0aa718ad16
silentninja <cyberdice113@gmail.com>
2019-11-26 23:03:39 -0700

    Added the ability to export lumin info by tag

--- Navi/plugins/export.py
+++ Navi/plugins/export.py
@@ -5,6 +5,8 @@ from .webapp_export import webapp_export
 from .consec_export import consec_export
 from .csv_export import csv_export
 from .lumin_export import lumin_export
+from .database import new_db_connection
+from .tag_export import tag_export
 
 @click.command(help="Export data into a CSV")
 @click.option('-assets', is_flag=True, help='Exports all Asset data into a CSV')
@@ -13,7 +15,10 @@ from .lumin_export import lumin_export
 @click.option('-consec', is_flag=True, help="Export Container Security Summary into a CSV")
 @click.option('-licensed', is_flag=True, help="Export a List of all the Licensed Assets")
 @click.option('-lumin', is_flag=True, help="Export all Asset data including ACR and AES into a CSV. This will take some time")
-def export(assets, agents, webapp, consec, licensed, lumin):
+@click.option('-bytag', is_flag=True, help="Export all assets by tag; Include ACR and AES into a CSV")
+@click.option('--c', default='', help="Export bytag with the following Category name")
+@click.option('--v', default='', help="Export bytag with the Tag Value; requires --c and Category Name")
+def export(assets, agents, webapp, consec, licensed, lumin, bytag, c, v):
     if assets:
         print("\nExporting your data now. Saving asset_data.csv now...\n")
         csv_export()
@@ -36,5 +41,24 @@ def export(assets, agents, webapp, consec, licensed, lumin):
 
     if lumin:
         print("\nExporting your data now. This could take some time.  300 Assets per minute max.")
-        print("Saving agent_lumin.csv now...\n")
+        print("Saving asset_lumin.csv now...\n")
         lumin_export()
+
+    if bytag:
+        if c == '':
+            print("Category is required.  Please use the --c command")
+
+        if v == '':
+            print("Value is required. Please use the --v command")
+
+        database = r"navi.db"
+        conn = new_db_connection(database)
+        with conn:
+            new_list = []
+            cur = conn.cursor()
+            cur.execute("SELECT asset_uuid from tags where tag_key='" + c + "' and tag_value='" + v + "';")
+
+            assets = cur.fetchall()
+            for asset in assets:
+                new_list.append(asset[0])
+        tag_export(new_list)
---
+++ Navi/plugins/tag_export.py
@@ -0,0 +1,51 @@
+import csv
+from .database import new_db_connection
+from .api_wrapper import request_data
+
+
+def tag_export(tag_list):
+    database = r"navi.db"
+    conn = new_db_connection(database)
+    with conn:
+
+        #Create our headers - We will Add these two our list in order
+        header_list = ["IP Address", "Hostname", "FQDN", "UUID", "First Found", "Last Found", "Operating System",
+                       "Mac Address", "Agent-UUID", "last Licensed Scan Date", 'Info', 'Low', 'Medium', 'High', 'Critical', 'Asset Exposure Score', 'Asset Criticality Score']
+        cur = conn.cursor()
+        cur.execute("SELECT * from assets;")
+
+        data = cur.fetchall()
+
+        #Crete a csv file object
+        with open('bytag_lumin.csv', mode='w') as csv_file:
+            agent_writer = csv.writer(csv_file, delimiter=',', quotechar='"')
+
+            #write our Header information first
+            agent_writer.writerow(header_list)
+
+            #Loop through each asset
+            for assets in data:
+                export_list = []
+                if assets[3] in tag_list:
+                    for atr in assets:
+                        # Cycle through the Database and populate the new list
+                        export_list.append(atr)
+
+                    asset_id = assets[3]  # Grab the UUID to make API calls
+
+                    try:
+                        asset_info = request_data('GET', '/workbenches/assets/' + asset_id + '/info')
+
+                        for vuln in asset_info['info']['counts']['vulnerabilities']['severities']:
+                            export_list.append(vuln["count"])  # Add the vuln counts to the new list
+
+                        try:
+                            export_list.append(asset_info['info']['exposure_score'])  # add the exposure score
+                            export_list.append(asset_info['info']['acr_score'])  # add the ACR
+                        except:
+                            pass
+
+                    except:
+                        print("Check your API keys or your internet connection")
+                    # write to the CSV
+                    agent_writer.writerow(export_list)
