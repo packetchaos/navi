4d78c370f9216db54b844e7ccd5a77372b4369b0
silentninja <cyberdice113@gmail.com>
2019-12-07 13:46:38 -0700

    Adjusted the setupfile to add a long description for Pypi deployment.

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup, find_packages
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='Navi Pro',
     version="5.0.0",
     description="A command-line interface to Tenable.io",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author="Casey Reid",
     author_email="itprofguru@gmail.com",
     url="https://github.com/packetchaos/Navi",
