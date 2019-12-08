e761427ae2d74e864787421b68c83bcacd24eacb
silentninja <cyberdice113@gmail.com>
2019-12-07 21:04:44 -0700

    Changed version # to refelct bug changes

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='Navi Pro',
-    version="5.0.1",
+    version="5.0.2",
     description="A command-line interface to Tenable.io",
     long_description=long_description,
     long_description_content_type='text/markdown',
