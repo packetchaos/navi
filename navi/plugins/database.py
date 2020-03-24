6043b752725af5413625cfea38a03e4abb2b2157
silentninja <cyberdice113@gmail.com>
2020-03-24 16:20:38 -0700

    5.1.4 - increase db timeouts to avoid db locks

--- navi/plugins/database.py
+++ navi/plugins/database.py
@@ -7,7 +7,7 @@ def new_db_connection(db_file):
     conn = None
     try:
         # A database file will be created if one doesn't exist
-        conn = sqlite3.connect(db_file)
+        conn = sqlite3.connect(db_file, timeout=15)
     except Error as E:
         print(E)
     return conn
