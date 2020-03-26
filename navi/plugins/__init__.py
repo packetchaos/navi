be7d22c5914d9fc200c9b13e4bcf4b6ea40635b2
silentninja <cyberdice113@gmail.com>
2020-03-26 09:18:48 -0700

    5.1.7 - Updated Tags to support chunking groups into 2000 IPs to respect the API limit.

--- navi/plugins/__init__.py
+++ navi/plugins/__init__.py
@@ -20,7 +20,8 @@ from .listen import listen
 from .keys import keys
 from .smtp import smtp
 from .mail import mail
-from .tag import tag
+#from .tag import tag
+from .new_tag import tag
 from .agroup import agroup
 
 
