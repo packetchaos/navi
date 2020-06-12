cfbe1267948e082b4008156f0553de9e9cd439c2
silentninja <cyberdice113@gmail.com>
2020-06-12 15:20:20 -0700

    5.1.29 - Fixed bug limiting WAS FQDNs in exports in the -summary option; Also reduced asset export from 5000 assets to 1000 to support larger containers

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup, find_packages
-
 from os import path
+
+
 this_directory = path.abspath(path.dirname(__file__))
+
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='navi Pro',
+    name='navi pro',
     version="5.1.29",
     description="A command-line interface to Tenable.io",
     long_description=long_description,
@@ -17,6 +19,11 @@ setup(
     license="GNUv3",
     keywords='tenable tenable_io navi tio, lumin, navi pro, tio cli, tenable io cli',
     packages=find_packages(exclude=['docs', 'tests']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
     install_requires=[
         'Click>=7.0',
         'requests', 'flask'
