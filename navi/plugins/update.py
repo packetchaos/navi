e948a0bb913b15584f9872e3cac7a3e654e45b8d
silentninja <cyberdice113@gmail.com>
2020-07-07 10:58:55 -0700

    5.1.33 - removed a few lines of useless code.  fixed a few coding issues

--- navi/plugins/update.py
+++ navi/plugins/update.py
@@ -1,7 +1,6 @@
 import click
 from .th_asset_export import asset_export
 from .th_vuln_export import vuln_export
-# from .vuln_export import vuln_export
 from sqlite3 import Error
 
 
@@ -9,12 +8,12 @@ from sqlite3 import Error
 @click.option('-assets', is_flag=True, help="Update the Agent data")
 @click.option('-vulns', is_flag=True, help="Update the Vulnerability Data data")
 @click.option('--days', default='30', help="Limit the download to X # of days")
-@click.option('--exid', default=' ', help="Download using a specified Export ID")
+@click.option('--exid', default='0', help="Download using a specified Export ID")
 @click.option('--threads', default=10, help="Control the threads to speed up or slow down downloads - (1-10)")
 def update(assets, vulns, days, exid, threads):
     # Limit the amount of threads to avoid issues
     if threads != 10:
-        print("\nUsing {} threads at your request".format(threads))
+        print("\nUsing {} thread(s) at your request".format(threads))
         if threads not in range(1, 11):
             print("Enter a value between 1 and 10")
             exit()
