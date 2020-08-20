3d8bca44a966db2e39a11dc3ca8e60f6b178b956
silentninja <cyberdice113@gmail.com>
2020-08-19 18:40:50 -0700

    6.0.0 - Migration to pytenable

--- navi/plugins/agent_group_export.py
+++ navi/plugins/agent_group_export.py
@@ -1,28 +1,29 @@
 import csv
 import time
-from .api_wrapper import request_data
+from .api_wrapper import tenb_connection
+
+tio = tenb_connection()
 
 
 def agent_group_export(group):
 
-    data = request_data('GET', '/scanners/1/agent-groups')
-
-    for group_info in data['groups']:
+    for group_info in tio.agent_groups.list():
         group_name = group_info['name']
 
         if group_name == group:
             group_id = group_info['id']
 
-            agent_data = request_data('GET', '/scanners/1/agent-groups/' + str(group_id) + '/agents')
-
             with open('agent_group_data.csv', mode='w') as csv_file:
                 agent_writer = csv.writer(csv_file, delimiter=',', quotechar='"')
 
-                header_list = ["Agent Name", "IP Address", "Platform", "Last connected", "Last scanned", "Status", "Plugin Feed ID"]
+                header_list = ["Agent Name", "IP Address", "Platform", "Last connected", "Last scanned", "Status",
+                               "Plugin Feed ID"]
+
                 agent_writer.writerow(header_list)
 
-                # cycle through the agents and display the useful information
-                for agent in agent_data['agents']:
+                agents = tio.agent_groups.details(group_id)
+
+                for agent in agents['agents']:
                     name = agent['name']
                     ip = agent['ip']
                     platform = agent['platform']
@@ -35,6 +36,7 @@ def agent_group_export(group):
                         scanned_time = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(last_scanned))
                     except KeyError:
                         scanned_time = "Not Yet Scanned"
+
                     status = agent['status']
 
                     agent_writer.writerow([name, ip, platform, connect_time, scanned_time, status, plugin_feed])
