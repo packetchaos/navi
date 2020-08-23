f4413e01eeed0c09b399310ac2ac7e3465a3654c
silentninja <cyberdice113@gmail.com>
2020-08-23 11:23:15 -0700

    6.0.0 - Code Clean up. Command Consolidation effort

--- navi/plugins/group.py
+++ navi/plugins/group.py
@@ -33,39 +33,35 @@ def get_group_id(group_name):
     return group_id
 
 
-@click.command(help="Create a group or Add/remove a user from a group")
-@click.option("-new", is_flag=True, help="Create a new Group. Required: --name")
-@click.option("-add", is_flag=True, help="Add a user to a group. Required: --user and --name")
-@click.option("--name", default='', help="The Name of the group")
-@click.option("--user", default='', help="The User Name to be added or removed")
-@click.option("-remove", is_flag=True, help="Remove a user from a group. Requires --name and user")
-def usergroup(new, add, name, user, remove):
+@click.group(help="Create a group or Add/remove a user from a group")
+def usergroup():
+    pass
 
-    if new:
-        if name == '':
-            print("\nYou must supply a Name for the group")
-            exit()
-        # Check to see if the group already exists
-        group_id = get_group_id(name)
-        if group_id == 0:
-            create_group(name)
-        else:
-            print("Your Group already exists. Hers the group id {}".format(group_id))
 
-    if add:
-        if user == '' or name == '':
-            print("\nYou must supply a username and group name")
-            exit()
+@usergroup.command("Create a new user group")
+@click.option("--name", default='', required=True, help="The Name of the group")
+def create(name):
+    # Check to see if the group already exists
+    group_id = get_group_id(name)
+    if group_id == 0:
+        create_group(name)
+    else:
+        print("Your Group already exists. Hers the group id {}".format(group_id))
 
-        user_id = get_user_id(user)
-        group_id = get_group_id(name)
-        add_users(user_id, group_id)
 
-    if remove:
-        if user == '' or name == '':
-            print("\nYou must supply a username and group name")
-            exit()
+@usergroup.command("Add a User to a user group")
+@click.option("--name", default='', required=True, help="The Name of the group")
+@click.option("--user", default='', required=True, help="The User Name to be added or removed")
+def add(name, user):
+    user_id = get_user_id(user)
+    group_id = get_group_id(name)
+    add_users(user_id, group_id)
 
-        user_id = get_user_id(user)
-        group_id = get_group_id(name)
-        remove_user(user_id, group_id)
+
+@usergroup.command("Remove a User from a user group")
+@click.option("--name", default='', required=True, help="The Name of the group")
+@click.option("--user", default='', required=True, help="The User Name to be added or removed")
+def remove(name, user):
+    user_id = get_user_id(user)
+    group_id = get_group_id(name)
+    remove_user(user_id, group_id)
