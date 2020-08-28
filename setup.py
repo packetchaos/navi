50462d141b21c158ae695f8b45f6b839eb7cb54e
silentninja <cyberdice113@gmail.com>
2020-08-28 11:15:35 -0700

    6.1.0 - issue with setup file

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import setup, find_packages
 from os import path
-from navi.plugins.api_wrapper import navi_version
-
-version = navi_version()
 
 
 this_directory = path.abspath(path.dirname(__file__))
@@ -12,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='navi pro',
-    version=version[5:],
+    version='6.1.0',
     description="A command-line interface to Tenable.io",
     long_description=long_description,
     long_description_content_type='text/markdown',
