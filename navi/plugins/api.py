abc45e5c5a7cd10e1cbb197812a54dd09850eb33
silentninja <cyberdice113@gmail.com>
2021-04-25 18:53:58 -0700

    6.4.1 - Added options to 'api' command including to return raw json instead of pprint

--- navi/plugins/api.py
+++ navi/plugins/api.py
@@ -6,11 +6,17 @@ from .error_msg import error_msg
 
 @click.command(help="Test the API ex: scans ")
 @click.argument('url')
-def api(url):
-
+@click.option('-raw', is_flag=True, help="Return raw Jason")
+@click.option('--limit', default=50, help="Change API Request Limit")
+@click.option('--offset', default=0, help="Change API Request Offset")
+def api(url, raw, limit, offset):
+    params = {"limit": limit, "offset": offset}
     try:
-        data = request_data('GET', url)
-        pprint.pprint(data)
+        data = request_data('GET', url, params=params)
+        if not raw:
+            pprint.pprint(data)
+        else:
+            click.echo(data)
 
     except Exception as E:
         error_msg(E)
