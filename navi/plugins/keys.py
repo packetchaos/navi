1e37569884896eb23b8d40bd978d3b024a4d9e99
silentninja <cyberdice113@gmail.com>
2021-07-12 17:15:35 -0700

    6.5.1 - Minor change to keys file

--- navi/plugins/keys.py
+++ navi/plugins/keys.py
@@ -1,7 +1,8 @@
 import click
 import getpass
 from .database import new_db_connection
-from .dbconfig import create_keys_table, create_diff_table, create_assets_table, create_vulns_table
+from .dbconfig import create_keys_table, create_diff_table, create_assets_table, create_vulns_table, create_sc_keys, \
+    create_compliance_table
 
 
 @click.command(help="Enter or Reset your Keys")
@@ -10,10 +11,12 @@ from .dbconfig import create_keys_table, create_diff_table, create_assets_table,
 @click.option("--secret_key", "--s", default="", help="Provide your Secret Key")
 def keys(clear, access_key, secret_key):
     # create all Tables when keys are added.
+    create_sc_keys()
     create_keys_table()
     create_diff_table()
     create_vulns_table()
     create_assets_table()
+    create_compliance_table()
 
     if access_key == "" or secret_key == "":
         click.echo("Hey you don't have any Keys!")
@@ -27,6 +30,7 @@ def keys(clear, access_key, secret_key):
     key_dict = (access_key, secret_key)
     database = r"navi.db"
     conn = new_db_connection(database)
+
     with conn:
         sql = '''INSERT or IGNORE into keys(access_key, secret_key) VALUES(?,?)'''
         cur = conn.cursor()
