ddd708a4838b563012de41a1b25e3eec53d198bd
silentninja <cyberdice113@gmail.com>
2021-07-29 20:57:14 -0700

    6.5

--- navi/plugins/query_export.py
+++ navi/plugins/query_export.py
@@ -17,6 +17,7 @@ def query_export(query, name):
         data = cur.fetchall()
 
         click.echo("I'm exporting {}.csv with your requested data".format(name))
+
         # Crete a csv file object
         with open('{}.csv'.format(name), mode='w') as csv_file:
             agent_writer = csv.writer(csv_file, delimiter=',', quotechar='"')
