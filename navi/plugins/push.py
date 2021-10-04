3b534ed5da7b72a4d9ac4c2ccc76b189413c9139
silentninja <cyberdice113@gmail.com>
2021-10-04 11:20:59 -0700

    6.7.1 -  Ability to push a file to a host

--- navi/plugins/push.py
+++ navi/plugins/push.py
@@ -46,22 +46,54 @@ def connect(user, host, password):
     return shell
 
 
+def scp(user, host, password, filename):
+    ssh_new_key_string = 'Are you sure you want to continue connecting'
+    scp_login_string = 'scp {} {}@{}:/'.format(filename, user, host)
+
+    shell = pexpect.spawn(scp_login_string)
+
+    return_code = shell.expect([pexpect.TIMEOUT, ssh_new_key_string, '[P|p]assword:'])
+
+    if return_code == 0:
+        print("error connecting")
+        exit()
+
+    if return_code == 1:
+        shell.sendline('yes')
+        second_return_code = shell.expect([pexpect.TIMEOUT, '[P|p]assword:'])
+
+        if second_return_code == 0:
+            print("error connecting")
+            return
+
+    shell.sendline(password)
+    shell.expect(pexpect.EOF)
+    response = shell.before
+    print(response.decode('utf-8'))
+    print()
+
+
 @click.command(help="Push a command to a linux target")
 @click.option('--command', default='', required=True, help="Command you want to run in double-quotes")
 @click.option('--target', default='', required=True, help="Target IP receiving the command")
 @click.option('--wait', is_flag=True, help="Wait for longer commands")
-def push(command, target, wait):
+@click.option('--file', default=None, help="Push a file to a target")
+def push(command, target, wait, file):
 
     try:
         credentials = db_query("select username, password from ssh;")
         user = credentials[0][0]
         password = credentials[0][1]
-        shell = connect(user, target, password)
-
-        if wait:
-            send_command(shell, command, quick=0)
+        if file:
+            scp(user, target, password, file)
         else:
-            send_command(shell, command, quick=1)
+            shell = connect(user, target, password)
+
+            if wait:
+                send_command(shell, command, quick=0)
+            else:
+                send_command(shell, command, quick=1)
+
     except Exception as E:
         click.echo("Please use the 'navi ssh' command to enter your ssh credentials\n "
                    "If you have, then this host may not be a Linux machine or your Credentials are not working\n"
