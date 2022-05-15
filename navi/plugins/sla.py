3aafe9c5f27f67cb83ab734a5b2e98b87ca7d998
silentninja <cyberdice113@gmail.com>
2022-05-14 20:45:35 -0700

    6.8.1

--- navi/plugins/sla.py
+++ navi/plugins/sla.py
@@ -2,13 +2,8 @@ import click
 from .database import new_db_connection, create_table, drop_tables, db_query
 
 
-def reset_sla():
-    click.echo("\nYou don't have any SLA data.  Enter your SLA now. \n")
-    critical = input("Enter the SLA in days for Critical CVE3 Vulns: ")
-    high = input("Enter the SLA in days for High CVE3 Vulns: ")
-    medium = input("Enter the SLA in days for Medium CVE3 Vulns: ")
-    low = input("Enter the SLA in days for Low CVE3 Vulns: ")
-
+def reset_sla(critical, high, medium, low):
+    print("\n Resetting your SLA Now\n")
     database = r"navi.db"
     conn = new_db_connection(database)
     drop_tables(conn, 'sla')
@@ -28,15 +23,11 @@ def reset_sla():
         cur.execute(sql, sla_info)
 
 
-@click.command(help="Enter or Overwrite your SLA information")
-@click.option("-reset", is_flag=True, help="reset your SLA")
-def sla(reset):
-    if reset:
-        click.echo("\nFor now, SLAs are calculated on export.  So rerun 'navi update fixed' to get accurate SLA results\n")
-        reset_sla()
+def print_sla():
     try:
         sla_data = db_query("select * from sla;")
         click.echo("\nHere is your Current SLA data")
+
         critical, high, medium, low = sla_data[0]
 
         click.echo("\n     Critical SLA: {}".format(critical))
@@ -44,4 +35,42 @@ def sla(reset):
         click.echo("     Medium SLA: {}".format(medium))
         click.echo("     Low SLA: {}\n".format(low))
     except:
-        reset_sla()
+        # on failure, lets set the defaults.
+        critical = 7
+        high = 14
+        medium = 30
+        low = 180
+        reset_sla(critical, high, medium, low)
+
+
+@click.command(help="Enter or Overwrite your SLA information")
+@click.option("-reset", is_flag=True, help="reset your SLA")
+@click.option("--critical", default='', help="Set your Critical Vulnerability SLA")
+@click.option("--high", default='', help="Set your High Vulnerability SLA")
+@click.option("--medium", default='', help="Set your Meduim SLA")
+@click.option("--low", default='', help="Set your Low SLA")
+def sla(reset, critical, high, medium, low):
+    if reset:
+
+        if critical == '' and high == '' and medium == '' and low == '':
+            print("You Entered Nothing, but choose to reset your SLA.  I'm using the Defaults")
+            # Set Defaults: user could only select one
+            reset_sla(7, 14, 30, 180)
+            print_sla()
+        else:
+            if critical == '':
+                critical = 7
+
+            if high == '':
+                high = 14
+
+            if medium == '':
+                medium = 30
+
+            if low == '':
+                low = 180
+
+            reset_sla(critical, high, medium, low)
+            print_sla()
+    else:
+        print_sla()
