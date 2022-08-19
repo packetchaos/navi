165cc7a9baae0518141182e2350f5a1a2c819262
silentninja <cyberdice113@gmail.com>
2022-08-18 17:28:30 -0700

    7.0.1 - added a secondary term for ease of use

--- navi/plugins/cancel.py
+++ navi/plugins/cancel.py
@@ -4,8 +4,8 @@ from .api_wrapper import request_data
 
 @click.command(help="Choose your export type --> assets: '-a' or vulns: '-v'")
 @click.argument('uuid')
-@click.option('-a', is_flag=True, help="Cancel a Asset export")
-@click.option('-v', is_flag=True, help="Cancel a Vulnerability Export")
+@click.option('-a', '-assets', is_flag=True, help="Cancel a Asset export")
+@click.option('-v', '-vulns', is_flag=True, help="Cancel a Vulnerability Export")
 def cancel(uuid, a, v):
     if not a and not v:
         click.echo("\n You need to signify which export type: '-a' or '-v'")
