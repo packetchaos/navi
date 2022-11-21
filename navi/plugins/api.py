5a972f4d9ba45d429ae11f8b123ce2660b7c923f
silentninja <cyberdice113@gmail.com>
2022-11-21 10:21:11 -0700

    7.1.14 - Added post to api command

--- navi/plugins/api.py
+++ navi/plugins/api.py
@@ -9,10 +9,15 @@ from .error_msg import error_msg
 @click.option('-raw', is_flag=True, help="Return raw Json")
 @click.option('--limit', default=50, help="Change API Request Limit")
 @click.option('--offset', default=0, help="Change API Request Offset")
-def api(url, raw, limit, offset):
+@click.option('-post', is_flag=True, help="Use POST instead of GET")
+def api(url, raw, limit, offset, post):
     params = {"limit": limit, "offset": offset}
     try:
-        data = request_data('GET', url, params=params)
+        if post:
+            data = request_data('POST', url, params=params)
+        else:
+            data = request_data('GET', url, params=params)
+
         if not raw:
             pprint.pprint(data)
         else:
